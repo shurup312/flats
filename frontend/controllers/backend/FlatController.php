146a28c5eee57cee6c568f7cfa6f77ec6a6975cb
shurup312 <bdelphi@mail.ru>
2014-11-08 18:05:25 +0300

    Чуть правок

--- frontend/controllers/backend/FlatController.php
+++ frontend/controllers/backend/FlatController.php
@@ -7,50 +7,45 @@ use common\controllers\BackendController;
 use Yii;
 use yii\web\Response;
 
-
 /**
  *  Flat controller
  */
-class FlatController extends BackendController
-{
-	public function actionIndex()
-	{
+class FlatController extends BackendController {
+
+	public function actionIndex () {
 		FlatAsset::register($this->view);
 		return $this->render($this->action->id);
 	}
 
-	public function actionList()
-	{
-		if (Yii::$app->request->isPost) {
-			Yii::$app->response->format = Response::FORMAT_JSON;
-			$model = new Flats();
-			return [
-				'data' => $model->find()->asArray()->all(),
-				'fields' => $model->labels()
-			];
+	public function actionList () {
+		if (!Yii::$app->request->isPost) {
+			return $this->renderPartial($this->action->id);
 		}
-		return $this->renderPartial($this->action->id);
+		Yii::$app->response->format = Response::FORMAT_JSON;
+		$model                      = new Flats();
+		return [
+			'data'   => $model->find()->asArray()->all(),
+			'fields' => $model->labels()
+		];
 	}
 
-	public function actionCreate()
-	{
-		if (Yii::$app->request->isPost) {
-			$model = new Flats();
-			$model->setAttributes(Yii::$app->request->getBodyParams());
-			$model->user_id = Yii::$app->user->id;
-			Yii::$app->response->format = Response::FORMAT_JSON;
-			if ($model->save()) {
-				return [
-					'hasErrors' => false,
-				];
-			} else {
-				return [
-					'hasErrors' => true,
-					'errors' => $model->getErrors(),
-				];
-			}
+	public function actionCreate () {
+		if (!Yii::$app->request->isPost) {
+			return $this->renderPartial($this->action->id);
 		}
+		$model = new Flats();
+		$model->setAttributes(Yii::$app->request->getBodyParams());
+		$model->user_id             = Yii::$app->user->id;
+		Yii::$app->response->format = Response::FORMAT_JSON;
+		if (!$model->save()) {
+			return [
+				'hasErrors' => true,
+				'errors'    => $model->getErrors(),
+			];
+		}
+		return [
+			'hasErrors' => false,
+		];
 
-		return $this->renderPartial($this->action->id);
 	}
 }
